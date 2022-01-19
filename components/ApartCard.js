3e9c2d81cb7b42d9ec306af7fdecf081e4ba7689
Bruno Oliveira <bedgar1@gmail.com>
2022-01-19 17:07:06 +0000

    website structure and first apartment component

---
+++ components/ApartCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+const ApartCard = () => {
+  return (
+    <div className="apart-container">
+      <div className="apart-image">
+        <Image
+          src="/assets/main01.jpg"
+          alt="Problem with this image"
+          layout="fill"
+        />
+      </div>
+      <div className="apart-detail">
+        <div className="flex-end">
+          <div className="apart-numb">4</div>
+        </div>
+        <div className="apart-title">Apartment #4</div>
+        <div className="apart-building">General Torres Builing</div>
+        <div className="apart-description">River view and shared balcony</div>
+        <div className="apart-btn">Apartment Details</div>
+      </div>
+    </div>
+  );
+};
+
+export default ApartCard;
